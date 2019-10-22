ba56602ea889d0ee1a4c3667a5deca3957aa68ae
Константин Дьяченко <konard@me.com>
2019-10-22 14:28:55 +0300

    Closes #1.

--- SetterBase[TResult].cs
+++ SetterBase[TResult].cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using Platform.Interfaces;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -7,7 +8,7 @@ namespace Platform.Setters
     /// <remarks>
     /// Must be class, not struct (in order to persist access to Result property value).
     /// </remarks>
-    public abstract class SetterBase<TResult>
+    public abstract class SetterBase<TResult> : ISetter<TResult>
     {
         protected TResult _result;
         public TResult Result => _result;
