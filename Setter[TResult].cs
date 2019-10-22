beb633684b954743bff4a544bb5e2dc5cf377def
Константин Дьяченко <konard@me.com>
2019-10-22 14:36:14 +0300

    Less code duplication. Setter<TResult> now has SetFirstAndReturnTrue and SetFirstAndReturnFalse methods.

--- Setter[TResult].cs
+++ Setter[TResult].cs
@@ -4,24 +4,9 @@
 
 namespace Platform.Setters
 {
-    public class Setter<TResult> : SetterBase<TResult>
+    public class Setter<TResult> : Setter<TResult, bool>
     {
-        public Setter(TResult defaultValue) : base(defaultValue) { }
-
+        public Setter(TResult defaultValue) : base(true, false, defaultValue) { }
         public Setter() { }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool SetAndReturnTrue(TResult value)
-        {
-            _result = value;
-            return true;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool SetAndReturnFalse(TResult value)
-        {
-            _result = value;
-            return false;
-        }
     }
 }
