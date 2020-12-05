ea8b586299e4504b1bd385f70e4a75ad80ca0c13
B1ade05 <52030630+B1ade05@users.noreply.github.com>
2020-12-05 16:30:56 +0300

    Update SetterBase.cs

--- csharp/Platform.Setters/SetterBase.cs
+++ csharp/Platform.Setters/SetterBase.cs
@@ -5,21 +5,60 @@ using Platform.Interfaces;
 
 namespace Platform.Setters
 {
+    /// <summary>
+    /// <para>Represents a range between minumum and maximum values.</para>
+    /// <para>Представляет диапазон между минимальным и максимальным значениями.</para>
+    /// </summary>
     /// <remarks>
     /// Must be class, not struct (in order to persist access to Result property value).
     /// </remarks>
     public abstract class SetterBase<TResult> : ISetter<TResult>
     {
+        /// <summary>
+        /// <para>Represents a range between minumum and maximum values.</para>
+        /// <para>Представляет диапазон между минимальным и максимальным значениями.</para>
+        /// </summary>
+        /// <remarks>
+        /// Must be class, not struct (in order to persist access to Result property value).
+        /// </remarks>
         protected TResult _result;
-
+        
+        /// <summary>
+        /// <para>Represents a range between minumum and maximum values.</para>
+        /// <para>Представляет диапазон между минимальным и максимальным значениями.</para>
+        /// </summary>
+        /// <remarks>
+        /// Must be class, not struct (in order to persist access to Result property value).
+        /// </remarks>
         public TResult Result => _result;
-
+        
+        /// <summary>
+        /// <para>Represents a range between minumum and maximum values.</para>
+        /// <para>Представляет диапазон между минимальным и максимальным значениями.</para>
+        /// </summary>
+        /// <remarks>
+        /// Must be class, not struct (in order to persist access to Result property value).
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected SetterBase() { }
-
+    
+        /// <summary>
+        /// <para>Represents a range between minumum and maximum values.</para>
+        /// <para>Представляет диапазон между минимальным и максимальным значениями.</para>
+        /// </summary>
+        /// <remarks>
+        /// Must be class, not struct (in order to persist access to Result property value).
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected SetterBase(TResult defaultValue) => _result = defaultValue;
-
+        
+        /// <summary>
+        /// <para>Represents a range between minumum and maximum values.</para>
+        /// <para>Представляет диапазон между минимальным и максимальным значениями.</para>
+        /// </summary>
+        /// <remarks>
+        /// Must be class, not struct (in order to persist access to Result property value).
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Set(TResult value) => _result = value;
     }
