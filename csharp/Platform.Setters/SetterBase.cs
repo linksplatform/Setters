184de094861d30db1ca1ee35df8a7cc3f4ea2f38
B1ade05 <52030630+B1ade05@users.noreply.github.com>
2021-01-24 19:12:32 +0300

    Update SetterBase.cs

--- csharp/Platform.Setters/SetterBase.cs
+++ csharp/Platform.Setters/SetterBase.cs
@@ -6,7 +6,7 @@ using Platform.Interfaces;
 namespace Platform.Setters
 {
     /// <summary>
-    /// <para>Represents a base implementation for an setter that allows you to set a passed value to it as the resulting value.</para>
+    /// <para>Represents a base implementation for an setter that allows you to set a passed value to it as the result value.</para>
     /// <para>Представляет базовую реализацию для установщика, который позволяет установить переданное ему значение в качестве результирующего значения.</para>
     /// </summary>
     /// <remarks>
