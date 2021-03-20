b7f629e83b9d5bd2096df9e10bdb665df731f347
B1ade05 <52030630+B1ade05@users.noreply.github.com>
2021-03-20 19:47:28 +0300

    Update Setter[TResult, TDecision].cs

--- csharp/Platform.Setters/Setter[TResult, TDecision].cs
+++ csharp/Platform.Setters/Setter[TResult, TDecision].cs
@@ -6,13 +6,11 @@ using System.Runtime.CompilerServices;
 namespace Platform.Setters
 {
     /// <summary>
-    /// <para>Represents a implementation for an setter that allows you to set a passed value to it as the result value.</para>
-    /// <para>Представляет реализацию для установщика, который позволяет установить переданное ему значение в качестве результирующего значения.</para>
+    /// <para>This setter implementation has additional methods that, simultaneously with setting the result value, return <typeparamref name = "TDecision"/> values indicating true or false.</para>
+    /// <para>В этой реализации установщика есть дополнительные методы, которые одновременно с установкой результирующего значения возвращают значения типа <typeparamref name="TDecision"/>, обозначающие истину или ложь.</para>
     /// </summary>
     /// <typeparam name="TResult"><para>The type of result value.</para><para>Тип результирующего значения.</para></typeparam>
-    /// <remarks>
-    /// Must be class, not struct (in order to persist access to Result property value).
-    /// </remarks>
+    /// <typeparam name="TDecision"><para>The type of value which will be used to make the decision.</para><para>Тип значения на основе которого будет приниматься решение.</para></typeparam>
     public class Setter<TResult, TDecision> : SetterBase<TResult>
     {
         private readonly TDecision _trueValue;
