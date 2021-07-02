f7f38171a2f80f8b43c299b99659b2b3223311e1
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-07-02 21:20:35 +0400

    Add files via upload

--- cpp/Platform.Setters.Tests/SetterTests.cpp
+++ cpp/Platform.Setters.Tests/SetterTests.cpp
@@ -1,24 +1,22 @@
 ﻿#include <gtest/gtest.h>
 #include <Platform.Setters.h>
-#include <array>
-#include <stdint.h>
 namespace Platform::Setters::Tests
 {
 		TEST(SettersTests, ParameterlessConstructedSetterTest)
 		{
-			Setter<std::int32_t> setter = Setter<std::int32_t>();
+			auto setter = Setter<std::int32_t>();
 			ASSERT_EQ(0, setter.Result());
 		}
 
 		TEST(SettersTests, ConstructedWithDefaultValueSetterTest)
 		{
-			Setter<std::int32_t> setter = Setter<std::int32_t>(9);
+			auto setter = Setter<std::int32_t>(9);
 			ASSERT_EQ(9, setter.Result());
 		}
 
 		TEST(SettersTests, MethodsWithBooleanReturnTypeTest)
 		{
-			Setter<std::int32_t> setter = Setter<std::int32_t>();
+			auto setter = Setter<std::int32_t>();
 			ASSERT_TRUE(setter.SetAndReturnTrue(1));
 			ASSERT_EQ(1, setter.Result());
 			ASSERT_FALSE(setter.SetAndReturnFalse(2));
@@ -31,7 +29,7 @@ namespace Platform::Setters::Tests
 
 		TEST(SettersTests, MethodsWithIntegerReturnTypeTest)
 		{
-			Setter<std::int32_t, std::int32_t> setter = Setter<std::int32_t, std::int32_t>(1, 0);
+			auto setter = Setter<std::int32_t, std::int32_t>(1, 0);
 			ASSERT_EQ(1, setter.SetAndReturnTrue(1));
 			ASSERT_EQ(1, setter.Result());
 			ASSERT_EQ(0, setter.SetAndReturnFalse(2));
