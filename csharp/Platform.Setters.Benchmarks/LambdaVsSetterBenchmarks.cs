07840506e8f53c95473c507762283c5c5486d3d8
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-11 09:09:03 +0600

    `Cleanup`

--- csharp/Platform.Setters.Benchmarks/LambdaVsSetterBenchmarks.cs
+++ csharp/Platform.Setters.Benchmarks/LambdaVsSetterBenchmarks.cs
@@ -1,53 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
-namespace Platform.Setters.Benchmarks
+using System;
+using System.Collections.Generic;
+
+namespace Platform.Setters.Benchmarks;
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class LambdaVsSetterBenchmarks
 {
-    [SimpleJob]
-    [MemoryDiagnoser]
-    public class LambdaVsSetterBenchmarks
+    private int Process(Func<IList<int>, int> func) => func(new List<int> { 0, 1, 2, 3 });
+
+    [Benchmark]
+    public int LambdaBenchmark()
     {
-        [GlobalSetup]
-        public static void Setup()
+        int result;
+        var trueValue = 1;
+        var falseValue = 2;
+        int Lambda(IList<int> list)
         {
-
+            result = list[0];
+            return trueValue;
         }
-
-        [GlobalCleanup]
-        public static void Cleanup()
-        {
-
-        }
-
-        int Process(Func<IList<int>, int> func)
-        {
-            return func(new List<int>{0,1,2,3,4,5,6,7,8,9});
-        }
-
-        [Benchmark]
-        public int LambdaBenchmark()
-        {
-            int result;
-            var trueValue = 1;
-            var falseValue = 2;
-            int Lambda(IList<int> list)
-            {
-                result = list[0];
-                return trueValue;
-            }
-            return Process(Lambda);
-        }
-
-        [Benchmark]
-        public int SetterBenchmark()
-        {
-            var setter = new Setter<int, int>(1, 2);
-            return Process(setter.SetFirstAndReturnTrue);
-        }
-
+        return Process(Lambda);
     }
 
+    [Benchmark]
+    public int SetterBenchmark()
+    {
+        var setter = new Setter<int, int>(1, 2);
+        return Process(setter.SetFirstAndReturnTrue);
+    }
 }
-
