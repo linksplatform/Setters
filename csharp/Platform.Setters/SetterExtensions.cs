ad8721d57723eb24736c738b7167479527f4c90b
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-16 19:22:08 +0600

    Add `SetterExtensions`

--- csharp/Platform.Setters/SetterExtensions.cs
+++ csharp/Platform.Setters/SetterExtensions.cs
@@ -7,43 +7,43 @@ namespace Platform.Setters
         public static TDecision SetFirstFromListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list)
         {
             setter.Set(list[0]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
 
         public static TDecision SetFirstFromFirstListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
         {
             setter.Set(list1[0]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
 
         public static TDecision SetSecondFromFirstListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
         {
             setter.Set(list1[1]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
 
         public static TDecision SetThirdFromFirstListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
         {
             setter.Set(list1[2]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
 
         public static TDecision SetFirstFromSecondListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
         {
             setter.Set(list2[0]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
 
         public static TDecision SetSecondFromSecondListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
         {
             setter.Set(list2[1]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
 
         public static TDecision SetThirdFromSecondListAndReturnTrue<TResult, TDecision>(this Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
         {
             setter.Set(list2[2]);
-            return setter.TrueValue;
+            return setter._trueValue;
         }
     }
 }
