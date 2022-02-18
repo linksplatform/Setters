957cfc380b8553209c7c653454bfff385cf661b5
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-18 23:44:04 +0600

    Update `SetterExtensions`

--- cpp/Platform.Setters/SetterExtensions.h
+++ cpp/Platform.Setters/SetterExtensions.h
@@ -4,43 +4,43 @@ namespace Platform::Setters
     class SetterExtensions
     {
     public:
-        static TDecision SetFirstFromListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list)
+        static TDecision SetFirstFromListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list)
         {
             setter.Set(list[0]);
             return setter.GetTrue();
         }
 
-        static TDecision SetFirstFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
+        static TDecision SetFirstFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list1, Interfaces::IArray<TResult> list2)
         {
             setter.Set(list1[0]);
             return setter.GetTrue();
         }
 
-        static TDecision SetSecondFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
+        static TDecision SetSecondFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list1, Interfaces::IArray<TResult> list2)
         {
             setter.Set(list1[1]);
             return setter.GetTrue();
         }
 
-        static TDecision SetThirdFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
+        static TDecision SetThirdFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list1, Interfaces::IArray<TResult> list2)
         {
             setter.Set(list1[2]);
             return setter.GetTrue();
         }
 
-        static TDecision SetFirstFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
+        static TDecision SetFirstFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list1, Interfaces::IArray<TResult> list2)
         {
             setter.Set(list2[0]);
             return setter.GetTrue();
         }
 
-        static TDecision SetSecondFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
+        static TDecision SetSecondFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list1, Interfaces::IArray<TResult> list2)
         {
             setter.Set(list2[1]);
             return setter.GetTrue();
         }
 
-        static TDecision SetThirdFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, IList<TResult> list1, IList<TResult> list2)
+        static TDecision SetThirdFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::IArray<TResult> list1, Interfaces::IArray<TResult> list2)
         {
             setter.Set(list2[2]);
             return setter.GetTrue();
