c819991d12f6a0f4685c791538d4486e02877686
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-19 13:34:45 +0600

    Remove unused extensions

--- cpp/Platform.Setters/SetterExtensions.h
+++ cpp/Platform.Setters/SetterExtensions.h
@@ -1,46 +1,45 @@
 namespace Platform::Setters
 {
-    template<typename TResult, typename TDecision>
     class SetterExtensions
     {
     public:
-        static TDecision SetFirstFromListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list)
-        {
-            setter.Set(list[0]);
-            return setter.GetTrue();
-        }
-
-        static TDecision SetFirstFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list1, Interfaces::CArray<TResult> list2)
+        template<typename TResult, typename TDecision>
+        static TDecision SetFirstFromFirstListAndReturnTrue(Setter<TResult, TDecision>& setter, Interfaces::CArray<TResult> auto&& list1, Interfaces::CArray<TResult> auto&& list2)
         {
             setter.Set(list1[0]);
             return setter.GetTrue();
         }
 
-        static TDecision SetSecondFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list1, Interfaces::CArray<TResult> list2)
+        template<typename TResult, typename TDecision>
+        static TDecision SetSecondFromFirstListAndReturnTrue(Setter<TResult, TDecision>& setter, Interfaces::CArray<TResult> auto&& list1, Interfaces::CArray<TResult> auto&& list2)
         {
             setter.Set(list1[1]);
             return setter.GetTrue();
         }
 
-        static TDecision SetThirdFromFirstListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list1, Interfaces::CArray<TResult> list2)
+        template<typename TResult, typename TDecision>
+        static TDecision SetThirdFromFirstListAndReturnTrue(Setter<TResult, TDecision>& setter, Interfaces::CArray<TResult> auto&& list1, Interfaces::CArray<TResult> auto&& list2)
         {
             setter.Set(list1[2]);
             return setter.GetTrue();
         }
 
-        static TDecision SetFirstFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list1, Interfaces::CArray<TResult> list2)
+        template<typename TResult, typename TDecision>
+        static TDecision SetFirstFromSecondListAndReturnTrue(Setter<TResult, TDecision>& setter, Interfaces::CArray<TResult> auto&& list1, Interfaces::CArray<TResult> auto&& list2)
         {
             setter.Set(list2[0]);
             return setter.GetTrue();
         }
 
-        static TDecision SetSecondFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list1, Interfaces::CArray<TResult> list2)
+        template<typename TResult, typename TDecision>
+        static TDecision SetSecondFromSecondListAndReturnTrue(Setter<TResult, TDecision>& setter, Interfaces::CArray<TResult> auto&& list1, Interfaces::CArray<TResult> auto&& list2)
         {
             setter.Set(list2[1]);
             return setter.GetTrue();
         }
 
-        static TDecision SetThirdFromSecondListAndReturnTrue(Setter<TResult, TDecision> setter, Interfaces::CArray<TResult> list1, Interfaces::CArray<TResult> list2)
+        template<typename TResult, typename TDecision>
+        static TDecision SetThirdFromSecondListAndReturnTrue(Setter<TResult, TDecision>& setter, Interfaces::CArray<TResult> auto&& list1, Interfaces::CArray<TResult> auto&& list2)
         {
             setter.Set(list2[2]);
             return setter.GetTrue();
