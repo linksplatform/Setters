a884c0390891ee4e6612ec7ece85e9013f97ee93
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-19 13:34:55 +0600

    Update `SetterExtensionsTests`

--- cpp/Platform.Setters.Tests/SetterExtensionsTests.cpp
+++ cpp/Platform.Setters.Tests/SetterExtensionsTests.cpp
@@ -1,97 +1,53 @@
+using TLinkAddress = std::int32_t;
+
 namespace Platform::Setters::Tests
 {
-    TEST(SetterExtensionsTests, SetFirstFromListAndReturnTrue)
-    {
-        std::int32_t list { 1,2,3 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetFirstFromListAndReturnTrue(list);
-        ASSERT_EQ(setter.GetTrue(), decision);
-        ASSERT_EQ(list[0], setter.Result());
-    }
-
     TEST(SetterExtensionsTests, SetFirstFromFirstListAndReturnTrue)
     {
-        std::int32_t list { 1,2,3 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetFirstFromFirstListAndReturnTrue(list, list);
-        ASSERT_EQ(setter.GetTrue(), decision);
-        ASSERT_EQ(list[0], setter.Result());
-    }
-
-    TEST(SetterExtensionsTests, SetSecondFromFirstListAndReturnTrue)
-    {
-        std::int32_t list { 1,2,3 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetSecondFromFirstListAndReturnTrue(list, list);
-        ASSERT_EQ(setter.GetTrue(), decision);
-        ASSERT_EQ(list[1], setter.Result());
-    }
-
-    TEST(SetterExtensionsTests, SetThirdFromFirstListAndReturnTrue)
-    {
-        std::int32_t list { 1,2,3 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetThirdFromFirstListAndReturnTrue(list, list);
-        ASSERT_EQ(setter.GetTrue(), decision);
-        ASSERT_EQ(list[2], setter.Result());
-    }
-
-    TEST(SetterExtensionsTests, SetFirstFromFirstListAndReturnTrue)
-    {
-        std::int32_t list1 { 1,2,3 };
-        std::int32_t list2 { 4,5,6 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetFirstFromFirstListAndReturnTrue(list1, list2);
+        TLinkAddress list1[] { 1,2,3 };
+        TLinkAddress list2[] { 4,5,6 };
+        Setter<TLinkAddress> setter {};
+        bool decision { SetterExtensions::SetFirstFromFirstListAndReturnTrue(setter, list1, list2) };
         ASSERT_EQ(setter.GetTrue(), decision);
         ASSERT_EQ(list1[0], setter.Result());
     }
 
     TEST(SetterExtensionsTests, SetSecondFromFirstListAndReturnTrue)
     {
-        std::int32_t list1 { 1,2,3 };
-        std::int32_t list2 { 4,5,6 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetSecondFromFirstListAndReturnTrue(list1, list2);
+        TLinkAddress list1[] { 1,2,3 };
+        TLinkAddress list2[] { 4,5,6 };
+        Setter<TLinkAddress> setter {};
+        bool decision { SetterExtensions::SetSecondFromFirstListAndReturnTrue(setter, list1, list2) };
         ASSERT_EQ(setter.GetTrue(), decision);
         ASSERT_EQ(list1[1], setter.Result());
     }
 
-    TEST(SetterExtensionsTests, SetFirstFromFirstListAndReturnTrue)
-    {
-        std::int32_t list1 { 1,2,3 };
-        std::int32_t list2 { 4,5,6 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetFirstFromFirstListAndReturnTrue(list1, list2);
-        ASSERT_EQ(setter.GetTrue(), decision);
-        ASSERT_EQ(list1[2], setter.Result());
-    }
-
     TEST(SetterExtensionsTests, SetFirstFromSecondListAndReturnTrue)
     {
-        std::int32_t list1 { 1,2,3 };
-        std::int32_t list2 { 4,5,6 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetFirstFromSecondListAndReturnTrue(list1, list2);
+        TLinkAddress list1[] { 1,2,3 };
+        TLinkAddress list2[] { 4,5,6 };
+        Setter<TLinkAddress> setter {};
+        bool decision { SetterExtensions::SetFirstFromSecondListAndReturnTrue(setter, list1, list2) };
         ASSERT_EQ(setter.GetTrue(), decision);
         ASSERT_EQ(list2[0], setter.Result());
     }
 
     TEST(SetterExtensionsTests, SetSecondFromSecondListAndReturnTrue)
     {
-        std::int32_t list1 { 1,2,3 };
-        std::int32_t list2 { 4,5,6 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetSecondFromSecondListAndReturnTrue(list1, list2);
+        TLinkAddress list1[] { 1,2,3 };
+        TLinkAddress list2[] { 4,5,6 };
+        Setter<TLinkAddress> setter {};
+        bool decision { SetterExtensions::SetSecondFromSecondListAndReturnTrue(setter, list1, list2) };
         ASSERT_EQ(setter.GetTrue(), decision);
         ASSERT_EQ(list2[1], setter.Result());
     }
 
     TEST(SetterExtensionsTests, SetThirdFromSecondListAndReturnTrue)
     {
-        std::int32_t list1 { 1,2,3 };
-        std::int32_t list2 { 4,5,6 };
-        auto setter = Setter<std::int32_t>();
-        bool decision = setter.SetThirdFromSecondListAndReturnTrue(list1, list2);
+        TLinkAddress list1[] { 1,2,3 };
+        TLinkAddress list2[] { 4,5,6 };
+        Setter<TLinkAddress> setter {};
+        bool decision { SetterExtensions::SetThirdFromSecondListAndReturnTrue(setter, list1, list2) };
         ASSERT_EQ(setter.GetTrue(), decision);
         ASSERT_EQ(list2[2], setter.Result());
     }
