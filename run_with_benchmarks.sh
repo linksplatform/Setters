57e11e61a1b0a63b3f4f64f52d937314346f91af
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-02-25 21:47:53 +0600

    Create run_with_benchmarks.sh

---
+++ run_with_benchmarks.sh
@@ -0,0 +1,13 @@
+cd Setters
+cmake_flags="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-10 -DLINKS_PLATFORM_BENCHMARKS=ON"
+cmake_build_dir="build"
+cd cpp && mkdir -p $cmake_build_dir && cd $cmake_build_dir
+conan install .. -pr=linksplatform --build=missing
+cmake .. $cmake_flags
+cmake --build .
+scan-build cmake --build .
+binaries=bin/*
+for binary in $binaries
+do
+   ./$binary
+done
