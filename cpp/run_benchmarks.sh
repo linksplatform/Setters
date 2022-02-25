9a8546e4ce1000069da07d18185c12d4357a16a5
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-25 22:05:06 +0600

    Update sh scripts for cpp

--- cpp/run_benchmarks.sh
+++ cpp/run_benchmarks.sh
@@ -1,6 +1,6 @@
 cmake_flags="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-10 -DLINKS_PLATFORM_BENCHMARKS=ON"
 cmake_build_dir="build"
-cd cpp && mkdir -p $cmake_build_dir && cd $cmake_build_dir
+mkdir -p $cmake_build_dir && cd $cmake_build_dir
 conan install .. -pr=linksplatform --build=missing
 cmake .. $cmake_flags
 cmake --build .
