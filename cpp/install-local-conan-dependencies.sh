be0a560cf913263a57fc22b5c2e2a62f02c8e198
Daulet Nassipkali <uralskdev@gmail.com>
2023-07-03 12:56:47 +0500

    Update install-local-conan-dependencies.sh

--- cpp/install-local-conan-dependencies.sh
+++ cpp/install-local-conan-dependencies.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/linksplatform/conan-center-index
-cd conan-center-index && git checkout only-development && cd recipes
-conan create platform.interfaces/0.2.0+ platform.interfaces/0.2.5@ -pr=linksplatform
+cd conan-center-index && cd recipes
+conan create platform.interfaces/all --version 0.3.41
